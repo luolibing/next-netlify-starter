2cbc4e74e82b920f6aba3d969b5112c3d8dfb6f2
Tim <397911353@qq.com>
2021-10-12 19:54:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="AnkerCloud go go go!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Netlify demo
         </p>
       </main>
 
